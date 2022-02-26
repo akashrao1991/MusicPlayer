f3751b460c17b64c48f8c8f85b2c2977d488307d
Akash Rao <akash@aclize.com>
2022-02-26 12:30:53 +0530

    working app till tracklist

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 /**
  * @format
  */
-
 import {AppRegistry} from 'react-native';
-import App from './app/App';
+import App from './app/new-app/App';
+// import App from './app/App';
 import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
+// import { TrackPlayerService } from './app/new-app/TrackPlayerService';
 
+// AppRegistry.registerComponent(appName, () => App)
 AppRegistry.registerComponent(appName, () => App);
+
+// TrackPlayer.registerPlaybackService(()=>require('./app/new-app/TrackPlayerService'))
+TrackPlayer.registerPlaybackService(() => require('./app/new-app/TrackPlayerService'))
